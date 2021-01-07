fe11642af444de5e8602e9281772671837acedd8
eduardomsj <eduardomsj@gmail.com>
2021-01-07 13:40:31 -0300

    Created Routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import StepsHeader from './StepsHeader';
+import './styles.scss';
+
+function Orders() {
+    return (
+        <div className="orders-container">
+            <StepsHeader />
+        </div>
+    );
+}
+
+export default Orders;
