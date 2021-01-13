1aa64477458961bfa22f63718cf1c85b9e953b62
eduardomsj <eduardomsj@gmail.com>
2021-01-13 00:00:43 -0300

    Created Routes and finished Home page

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,20 +1,30 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export default function Header() {
+  const navigation = useNavigation();
+  
+  const handleOnPress = () => {
+    navigation.navigate('Home');
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}>DS Delivery</Text>      
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require('../assets/logo.png')} />
+        <Text style={styles.text}>DS Delivery</Text>      
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#DA5C5C',
-    height: 90,
-    paddingTop: 50,
+    height: 70,
+    paddingTop: 30,
     flexDirection: 'row',
     justifyContent: 'center'
   },
---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../Header';
+
+export default function Orders() {
+  return (
+    <>
+        <Header />
+        <View>
+            <Text>LISTAGEM DE PRODUTOS</Text>
+        </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
