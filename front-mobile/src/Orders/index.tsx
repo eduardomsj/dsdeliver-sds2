f69451072a95652cb838cb8c1045ddb9b0c5d7ef
eduardomsj <eduardomsj@gmail.com>
2021-01-14 20:23:06 -0300

    Created Orders list

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, View } from 'react-native';
 import Header from '../Header';
+import OrderCard from '../OrderCard';
 
 export default function Orders() {
   return (
     <>
         <Header />
-        <View>
-            <Text>LISTAGEM DE PRODUTOS</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <OrderCard />
+            <OrderCard />
+            <OrderCard />
+            <OrderCard />
+        </ScrollView>
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingRight: '5%',
+    paddingLeft: '5%',
+  }
+});
